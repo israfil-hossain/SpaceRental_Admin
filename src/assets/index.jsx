48180f980da4d5e7b88d97dacaff0b91a3ce3bb7
Israfil-Digi360 <mailto:israfil.hossain@digigate360.com>
2023-12-27 22:38:12 +0600

    cleanproject:Clean Old Code

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,16 +1,10 @@
-import wave from "./wave.png"; 
-
-import wave2 from "./wave2.png";
-import subscribe from "./Subscriber.gif";
-import Profile from "./Profile.svg";
 import logo from "./Stockams.png"; 
 import profile from "./images/profile.svg";
-
+import notfound from "./images/notfound.gif"; 
+import loader from "./images/loader.gif"; 
 export {
-    wave,
-    wave2,
-    subscribe,
-    Profile, 
+    notfound, 
+    loader,
     logo,
     profile,
 }
--- src/components/common/CommonProgress.jsx
+++ src/components/common/CommonProgress.jsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import React from "react";
-import { loader } from "../../assets/image";
+import { loader } from "../../assets";
+
 
 export const CommonProgress = () => {
   return (
